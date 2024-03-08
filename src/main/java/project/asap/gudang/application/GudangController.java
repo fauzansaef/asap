26d6cb452f0edf5f02ac881202ee4a705d5796b4
Fauzan Saef <fauzansaef@gmail.com>
2024-03-08 16:06:27 +0700

    fix : controller gudang

--- src/main/java/project/asap/gudang/application/GudangController.java
+++ src/main/java/project/asap/gudang/application/GudangController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import project.asap.gudang.domain.dto.GudangRequest;
 import project.asap.gudang.domain.entity.Gudang;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/gudangs")
 @Tag(name = "Gudang", description = "API Transaction data Gudang (Pengarsipan)")
@@ -25,22 +27,22 @@ public class GudangController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(Long id) {
+    public ResponseEntity<?> getById(@PathVariable Long id) {
         return new ResponseEntity<>(gudangService.getById(id), HttpStatus.OK);
     }
 
     @PostMapping("")
-    public ResponseEntity<?> save(GudangRequest gudangRequest) {
+    public ResponseEntity<?> save(@RequestBody @Valid GudangRequest gudangRequest) {
         return new ResponseEntity<>(gudangService.save(gudangRequest), HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<?> update(Long id, GudangRequest gudangRequest) {
+    public ResponseEntity<?> update(@PathVariable Long id,@RequestBody @Valid GudangRequest gudangRequest) {
         return new ResponseEntity<>(gudangService.update(id, gudangRequest), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(Long id) {
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         return new ResponseEntity<>(gudangService.delete(id), HttpStatus.OK);
     }
 
