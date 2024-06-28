a603746b62281e63f744e0c8678d840c4eb2347a
erwinadinugraha <erwinadinugraha@gmail.com>
2024-06-28 09:45:43 +0700

    add arsip by excel

--- src/main/java/project/asap/arsip/application/ArsipController.java
+++ src/main/java/project/asap/arsip/application/ArsipController.java
@@ -2,9 +2,15 @@ package project.asap.arsip.application;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import project.asap.arsip.domain.dto.TambahArsipRequest;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/api/v1/arsip")
@@ -44,5 +50,16 @@ public class ArsipController {
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable Long id) {
         return ResponseEntity.ok(arsipService.delete(id));
+    }
+
+    @PostMapping(value ="/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            InputStream inputStream = file.getInputStream();
+            arsipService.saveFromExcel(inputStream);
+            return ResponseEntity.ok().body("File uploaded successfully");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file");
+        }
     }
 }
